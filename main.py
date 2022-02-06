6dac093a878f49f2189117cc9e32ced5ebbf2274
Michael Kronovet <michaelkronovet@Michaels-MacBook-Pro.local>
2022-02-06 15:38:17 -0500

    fixed capital gains taxes

--- main.py
+++ main.py
@@ -58,6 +58,7 @@ if company_public_state=="Private":
      min_value=float(0), max_value=float(100), value=0.15) 
 
     share_value = preferred_price*share_count
+    fair_market_share_value = strike_price*share_count
 
 else:
     yearly_dilution = float(0)
@@ -85,7 +86,7 @@ future_valuation = st.number_input(
      min_value=0, max_value=MAX_INT, value=50000000000)
 
 
-def calculate_tax_amount(tax_table, income):
+def calculate_income_tax_amount(tax_table, income):
     tax_total = 0
     for _, row in tax_table.iterrows():
         if income < row['end']:
@@ -95,6 +96,14 @@ def calculate_tax_amount(tax_table, income):
             tax_total += row['end']*row['tax_rate']
             income = income - row['end']
     return tax_total
+
+def calculate_capital_gains_tax_amount(income, amount_to_be_taxed_on):
+    if 40400 < income and income <= 445850:
+        print("here it goes: ", amount_to_be_taxed_on*0.1)
+        return amount_to_be_taxed_on*0.15
+    if income > 445850:
+        return amount_to_be_taxed_on*0.2
+    return 0
 
 # If company grows 20% a year, then it's company_growth_rate = 1.2
 company_growth_rate = (future_valuation/current_valuation)**(1/future_examination_len)
@@ -120,9 +129,9 @@ for yr in range(1, int(future_examination_len+1)):
         'vested_unsell-able_shares': 0,
         'unvested_shares': 0,
         'execution_cost': 0,
-        'taxed_money': 0,
+        'income_taxes_owed': 0,
         'total_accumulated_stock_value': 0,
-        'taxes_owed_on_vested_stock_increases': 0
+        'capital_gains_taxes_owed': 0
     })
     # IPO has happened
     if yr > ipo_distance:
@@ -137,21 +146,36 @@ for yr in range(1, int(future_examination_len+1)):
     if company_public_state=='Public':
         taxable_stock_income = yr_outcomes['sell-able_shares']
     else:
-        taxable_stock_income = new_equity_valuation - strike_price*(share_count/vesting_length)
+        new_fair_market_equity_valuation = (fair_market_share_value/vesting_length)*dilution_amount*(company_growth_rate**(yr))
+        if yr <= ipo_distance:
+            taxable_stock_income = new_fair_market_equity_valuation - strike_price*(share_count/vesting_length)
+        else:
+            taxable_stock_income = new_equity_valuation - strike_price*(share_count/vesting_length)
         yr_outcomes['execution_cost'] = strike_price*(share_count/vesting_length)
     
     yr_outcomes['unvested_shares'] = (share_value/vesting_length)*(vesting_length-yr)*dilution_amount*(company_growth_rate**(yr))
     total_taxable_income = yr_outcomes['real_money'] + taxable_stock_income
-    yr_outcomes['taxed_money'] = calculate_tax_amount(federal_income_tax_brackets, total_taxable_income)
+    yr_outcomes['income_taxes_owed'] = calculate_income_tax_amount(federal_income_tax_brackets, total_taxable_income)
 
-    yr_outcomes['real_money_post_fees'] = yr_outcomes['real_money'] - yr_outcomes['taxed_money'] - yr_outcomes['execution_cost']
+    yr_outcomes['real_money_post_fees'] = yr_outcomes['real_money'] - yr_outcomes['income_taxes_owed'] - yr_outcomes['execution_cost']
 
     yr_outcomes['total_accumulated_stock_value'] = new_equity_valuation*yr
 
-    # pay tax on equity 
-    # pay money on base+total accumulated value - (base+ new_equity_valuation - strike_price*(share_count/vesting_length))
-    # Assumes that we will pay the tax on difference between preferred and strike price now, but in reality we pay it later when we sell.
-    yr_outcomes['taxes_owed_on_vested_stock_increases'] = calculate_tax_amount(federal_income_tax_brackets, yr_outcomes['total_accumulated_stock_value']-yr_outcomes['execution_cost']-prev_total_accumulated_stock_value+yr_outcomes['real_money']) - yr_outcomes['taxed_money']
+    if company_public_state=='Public' or yr > ipo_distance:
+        print("Cap gains metrics for yr: ", yr)
+        print(yr_outcomes['total_accumulated_stock_value'])
+        print(new_equity_valuation)
+        print(prev_total_accumulated_stock_value)
+
+        print("VALUES", yr_outcomes['total_accumulated_stock_value'] - new_equity_valuation - prev_total_accumulated_stock_value)
+        
+        yr_outcomes['capital_gains_taxes_owed'] = calculate_capital_gains_tax_amount(yr_outcomes['real_money']+taxable_stock_income, 
+            yr_outcomes['total_accumulated_stock_value'] - new_equity_valuation - prev_total_accumulated_stock_value)
+    else:
+        # diff in preferred stock value - new_fair_market_equity_valuation
+        pref_stock_less_fmv_stock = new_equity_valuation - new_fair_market_equity_valuation
+        yr_outcomes['capital_gains_taxes_owed'] = calculate_capital_gains_tax_amount(yr_outcomes['real_money']+taxable_stock_income, 
+            yr_outcomes['total_accumulated_stock_value'] - new_equity_valuation - prev_total_accumulated_stock_value + pref_stock_less_fmv_stock)
 
     prev_total_accumulated_stock_value = yr_outcomes['total_accumulated_stock_value']
 
@@ -166,16 +190,16 @@ st.title(f"Company Aggregate Compensation Over {future_examination_len} Years")
 total_accumulated_base = output_table['real_money'].sum()
 total_accumulated_equity = output_table['total_accumulated_stock_value'].iloc[-1]
 total_base_less_fees_and_taxes = output_table['real_money_post_fees'].sum()
-taxes_owed_when_selling_all_shares = output_table['taxes_owed_on_vested_stock_increases'].sum()
-total_taxes_already_payed = output_table['taxed_money'].sum()
+capital_gains_taxes_owed_when_selling_all_shares = output_table['capital_gains_taxes_owed'].sum()
+total_taxes_already_payed = output_table['income_taxes_owed'].sum()
 total_execution_fees = output_table['execution_cost'].sum()
 st.table(data=pd.DataFrame({
     'total_accumulated_base': [total_accumulated_base],
     'total_accumulated_equity': [total_accumulated_equity],
     'total_base_less_fees_and_taxes': [total_base_less_fees_and_taxes],
-    'taxes_owed_when_selling_all_shares': [taxes_owed_when_selling_all_shares],
+    'capital_gains_taxes_owed_when_selling_all_shares': [capital_gains_taxes_owed_when_selling_all_shares],
     'total_taxes_already_payed': [total_taxes_already_payed],
     'total_execution_fees': [total_execution_fees]}).T.style.format("${:,.2f}"))
 
 
-st.title(f"Total net wealth from this job after {future_examination_len} years (selling all shares and paying all taxes) is {'${:,.2f}'.format(total_base_less_fees_and_taxes+total_accumulated_equity-taxes_owed_when_selling_all_shares)}")
+st.title(f"Total net wealth from this job after {future_examination_len} years (selling all shares and paying all taxes) is {'${:,.2f}'.format(total_base_less_fees_and_taxes+total_accumulated_equity-capital_gains_taxes_owed_when_selling_all_shares)}")
